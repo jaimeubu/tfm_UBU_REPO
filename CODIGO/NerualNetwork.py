1b3968447b551f11249aa0407f868e8c5c3d89a4
jaume <jvazquez@dgtic.caib.es>
2023-04-30 19:53:56 +0200

    Ajuste hiperparametros red neuronal

--- CODIGO/NerualNetwork.py
+++ CODIGO/NerualNetwork.py
@@ -1,9 +1,11 @@
+from sklearn.model_selection import RandomizedSearchCV 
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from imblearn.under_sampling import RandomUnderSampler
+from scipy.stats import randint as sp_randint
 
 # Cargar los datos desde un archivo o DataFrame
 data = pd.read_csv(r"C:\Users\gta3_\Desktop\Master_ing_infor\TFM\CODIGO\heart_disease_health_indicators_BRFSS2015.csv")
@@ -21,13 +23,24 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Crear un modelo de redes neuronales
-model = MLPClassifier(hidden_layer_sizes=(50,50), max_iter=1000)
+model = MLPClassifier()#hidden_layer_sizes=(50,50), max_iter=1000)
+
+# Hiperparametros a ajustar
+parameter_space = {
+    'hidden_layer_sizes': [(50,50,50), (50,100,50), (50,)],
+    'activation': ['tanh'],
+    'solver': ['sgd'],
+    'alpha': [0.05],
+    'learning_rate': ['adaptive'],
+    'max_iter': sp_randint(200, 2000)
+}
+clf = RandomizedSearchCV(model, parameter_space, n_jobs=-1, cv=3)
 
 # Ajustar el modelo a los datos de entrenamiento
-model.fit(X_train, y_train)
+clf.fit(X_train, y_train)
 
 # Realizar predicciones en los datos de prueba
-y_pred = model.predict(X_test)
+y_pred = clf.best_estimator_.predict(X_test)
 
 # Evaluar la precisión del modelo
 accuracy = accuracy_score(y_test, y_pred)
@@ -36,3 +49,4 @@ print(f'Precisión del modelo de redes neuronales: {accuracy:.4f}')
 
 matrix = confusion_matrix(y_test, y_pred)
 print(matrix)
+print(clf.best_params_)
